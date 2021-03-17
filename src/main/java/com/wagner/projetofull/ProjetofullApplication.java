95567d7d0de76f13aeba15c7b32b847f64c0baff
Wagner Martins <wagnermartinssantana@gmail.com>
2021-03-17 14:18:46 -0300

    Iniciando o Projeto

---
+++ src/main/java/com/wagner/projetofull/ProjetofullApplication.java
@@ -0,0 +1,13 @@
+package com.wagner.projetofull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetofullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetofullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wagner/projetofull/ProjetofullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wagner.projetofull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetofullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
