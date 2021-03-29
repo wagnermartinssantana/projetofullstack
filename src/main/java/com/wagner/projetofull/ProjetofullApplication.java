f25611b5d1f06a9a68f6d6d4e03f698b24c6b14c
Wagner Martins <wagnermartinssantana@gmail.com>
2021-03-29 17:05:17 -0300

    Alteração no tipo enumerado uma forma funcional

--- src/main/java/com/wagner/projetofull/ProjetofullApplication.java
+++ src/main/java/com/wagner/projetofull/ProjetofullApplication.java
@@ -8,8 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.wagner.projetofull.domain.Categoria;
+import com.wagner.projetofull.domain.Cidade;
+import com.wagner.projetofull.domain.Cliente;
+import com.wagner.projetofull.domain.Endereco;
+import com.wagner.projetofull.domain.Estado;
 import com.wagner.projetofull.domain.Produto;
+//import com.wagner.projetofull.domain.enums.TipoCliente;
 import com.wagner.projetofull.repositories.CategoriaRepository;
+import com.wagner.projetofull.repositories.CidadeRepository;
+import com.wagner.projetofull.repositories.ClienteRepository;
+import com.wagner.projetofull.repositories.EnderecoRepository;
+import com.wagner.projetofull.repositories.EstadoRepository;
 import com.wagner.projetofull.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -27,6 +36,19 @@ public class ProjetofullApplication  implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
 	
 	
 	public static void main(String[] args) {
@@ -54,12 +76,41 @@ public class ProjetofullApplication  implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1 , cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		
+		Estado est1 = new Estado (null,"Minas Gerais");
+		Estado est2 = new Estado (null,"São Paulo");
+		
+		Cidade c1 = new Cidade (null,"Uberlândia" , est1);
+		Cidade c2 = new Cidade (null,"São Paulo" , est2);
+		Cidade c3 = new Cidade (null,"Campinas" , est2);
+
+				
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		
+
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
 		
+		Cliente cli1 = new Cliente(null, "Maria Silva","maria@gmail.com","01952834652","PESSOA FISICA");
+		
+		cli1.getTelefones().addAll(Arrays.asList("975464151" , "975467474"));
+		
+		Endereco e1 = new Endereco(null, "Rua flores", "40", "Apt 102", "Jardim", "386656489", cli1, c1);
+
+		Endereco e2 = new Endereco(null, "Rua matos", "40", "Apt 102", "São Paulo", "386656489", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
 		
 	}
 	
---
+++ src/main/java/com/wagner/projetofull/domain/enums/TipoCliente.java
@@ -0,0 +1,50 @@
+package com.wagner.projetofull.domain.enums;
+
+public enum TipoCliente {
+	
+	PESSOAFISICA,
+	PESSOAJURIDICA;
+
+	/*
+	/*static pq nao estava dando ok
+	
+	private static Integer cod; 
+	private String descricao;
+	
+	
+	private TipoCliente(Integer cod, String descricao) {
+		this.descricao = descricao;
+	}
+
+
+	public Integer getCod() {
+		return cod;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+
+	public static TipoCliente toEnum(Integer Cod) {
+		
+		if(cod == null) {
+			return null;
+		}
+		
+		for (TipoCliente x : TipoCliente.values()) {
+			if (Cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Id inválido: " + cod); 
+		
+		
+	}
+
+	 */
+	
+}
