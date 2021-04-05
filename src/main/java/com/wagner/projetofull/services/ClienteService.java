73ed0953be1666c1e764a4b61b9c7165b50536b8
Wagner Martins <wagnermartinssantana@gmail.com>
2021-04-05 17:50:15 -0300

    Json Endpoint /clientes/id disponível

---
+++ src/main/java/com/wagner/projetofull/services/ClienteService.java
@@ -0,0 +1,27 @@
+package com.wagner.projetofull.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wagner.projetofull.domain.Cliente;
+import com.wagner.projetofull.repositories.ClienteRepository;
+import com.wagner.projetofull.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+								"Objeto não encontrado! ID:" + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+}
